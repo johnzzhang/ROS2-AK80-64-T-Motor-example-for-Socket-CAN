b40868baa9cd1ca92cba04d5881d0c3dde652fb9
johnzzhang <johnz@mit.edu>
2022-10-14 17:12:40 -0400

    Code for two motors. Code for one works without issue.

--- tmotor/run_motor.py
+++ tmotor/run_motor.py
@@ -25,32 +25,50 @@ class MotorNode(Node):
             10) # queue length
 
         # 0.5 second timeout is too fast for PCAN
-        self.motor1 = CanMotorController(can_socket='can0', motor_id=0x01, motor_type='AK80_6_V2',socket_timeout=1)
+        self.motor1 = CanMotorController(can_socket='can0', motor_id=0x01, motor_type='AK80_6_V2',socket_timeout=2)
+        time.sleep(1)
+        self.motor2 = CanMotorController(can_socket='can0', motor_id=0x02, motor_type='AK80_6_V2',socket_timeout=2)
         time.sleep(1)
 
         print('Enabling motor...')
         self.motor1.enable_motor()
         time.sleep(1)
+        self.motor2.enable_motor()
+        time.sleep(1)
 
         print('Zeroing motor position...')
         self.motor1.set_zero_position()
+        time.sleep(1)
+        self.motor2.set_zero_position()
         time.sleep(1)
 
     def motor_callback(self, msg):
         max_vel = 0
         ff = 0
-        ref = msg.ref # desired position in degrees
-        Kp = msg.kp
-        Kd = msg.kd
+        ref1 = msg.ref1 # desired position in degrees
+        Kp1 = msg.kp1
+        Kd1 = msg.kd1
+        ref2 = msg.ref2 # desired position in degrees
+        Kp2 = msg.kp2
+        Kd2 = msg.kd2
 
         motor_output_msg = MotorOutput()
-        pos, vel, torq = self.motor1.send_deg_command(ref,max_vel,Kp,Kd,ff)
+        pos1, vel1, torq1 = self.motor1.send_deg_command(ref1,max_vel,Kp1,Kd1,ff)
+        time.sleep(.1)
+        pos2, vel2, torq2 = self.motor2.send_deg_command(ref2,max_vel,Kp2,Kd2,ff)
+        time.sleep(.1)
 
-        motor_output_msg.pos = pos
-        motor_output_msg.vel = vel
-        motor_output_msg.torq = torq
+        motor_output_msg.pos1 = pos1
+        motor_output_msg.vel1 = vel1
+        motor_output_msg.torq1 = torq1
+
+        motor_output_msg.pos2 = pos2
+        motor_output_msg.vel2 = vel2
+        motor_output_msg.torq2 = torq2
 
         self.motor_pub.publish(motor_output_msg)
+
+
 
 def main():
     rclpy.init()
